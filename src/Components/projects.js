87b10ab7ef3a923b253708b752bf44b37f3c089c
hyunseung <charlie9807@kaist.ac.kr>
2024-02-23 16:54:42 +0900

    project

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import './components.css'
+import projectData from '../Data/projects.json'
+
 
 export const Projects = (props) => {
     return(
         <div className="projects">
             <div className='title'>Projects</div>
+            <div className='projectlist'>
+                {projectData.map(project => (
+                        <div className='project' key={project.title}>
+                                <img src={'images/' + project.image} alt={project.title}/>
+                                <div className='projectcontents'>
+                                    <span className='title'>
+                                        <span>{project.title}</span>
+                                        <span className='year'> ({project.year})</span>
+                                    </span>
+                                    <div className='authors'>{project.people.map((person, index) => 
+                                        <span className='authorholder'>
+                                            <span className={project.highlight_people.includes(index) ? "highlight_person" : "person"}>{person}</span>
+                                            {index < project.people.length - 1 ? ', ' : ''}
+                                        </span>
+                                    )}</div>
+                                    <div className='description'>{project.description}</div>
+                                </div>
+
+                        </div>
+                    ))}
+            </div>
         </div>
     )
 }
--- src/Components/publications.js
+++ src/Components/publications.js
@@ -9,26 +9,29 @@ export const Publications = (props) => {
             <div className='publicationlist'>
                 {publicationData.map(publication => (
                     <div className='publication' key={publication.title}>
-                        <div className='maininfo'>
-                            <div className='title'>{publication.title}</div>
-                            <div className='authors'>{publication.authors.map((author, index) => 
-                                <span className='authorholder'>
-                                    <span className={publication.highlight_author.includes(index) ? "highlight_author" : "author"}>{author}</span>
-                                    {index < publication.authors.length - 1 ? ', ' : ''}
-                                </span>
-                            )}</div>
-                            <div className='awards'>{publication.awards.map(award =>
-                                <div className='award'>{award}
-                            </div>)}</div>
+                        <div className='info'>
+                            <div className='maininfo'>
+                                <div className='title'>{publication.title}</div>
+                                <div className='authors'>{publication.authors.map((author, index) => 
+                                    <span className='authorholder'>
+                                        <span className={publication.highlight_author.includes(index) ? "highlight_author" : "author"}>{author}</span>
+                                        {index < publication.authors.length - 1 ? ', ' : ''}
+                                    </span>
+                                    )}
+                                </div>
+                            </div>
+                            <div className='subinfo'>
+                                <span className='booktitle'>{publication.booktitle}</span>
+                                {publication.state ? <span className='state'>{publication.state}</span> : null}
+                            </div>
+                        </div>
+                        <div className='awards'>{publication.awards.map(award =>
+                                <span className='award'>🏆 {award}</span>)}
+                            </div>
                         <div className='links'>
                             {publication.url ? <a href={publication.url}>DOI</a> : null}
                             {publication.pdf ? <a href={publication.pdf}>PDF</a> : null}
                             {publication.video ? <a href={publication.video}>VIDEO</a> : null}
-                        </div>
-                        </div>
-                        <div className='otherinfo'>
-                            <div className='booktitle'>{publication.booktitle}</div>
-                            {publication.state ? <div className='state'>{publication.state}</div> : null}
                         </div>
                     </div>
                 ))}
