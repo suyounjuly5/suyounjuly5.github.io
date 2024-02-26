56e72cf025c277d220bbdd1f3a3e0526ed36526c
Hyunseung Lim <charlie9807@kaist.ac.kr>
2024-02-26 21:45:02 +0900

    hamburger btn

--- src/Components/topbar.js
+++ src/Components/topbar.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './components.css'
+import './topbar.css'
 
 export const Topbar = (props) => {
     return(
         <nav className='topbar'>
             <div className='contents'>
                 <img className='logo' src="logo192.png" alt="Logo" />
-                <div className='buttons'>
+                <input className="burger-check" type="checkbox" id="burger-check" /><label className="burger-icon" htmlFor="burger-check"><span className="burger-sticks"></span></label>
+                <div className='menu'>                         
+                    <a href="#about" className='hamburger-bar'>About Me</a>
+                    <a href="#projects" className='hamburger-bar'>Projects</a>
+                    <a href="#publications" className='hamburger-bar'>Publications</a>
+                    <a href='PDF/CV.pdf' className='hamburger-bar'>CV</a>
+                </div>
+                <div className='links'>
                     <a href="#about">About Me</a>
                     <a href="#projects">Projects</a>
                     <a href="#publications">Publications</a>
